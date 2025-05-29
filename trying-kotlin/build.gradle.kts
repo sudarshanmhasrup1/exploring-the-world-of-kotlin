c3408db3a7cfcf677f5433098c1491ae305e808a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-29 15:44:06 +0530

    Define repository source in build.gradle.kts of trying-kotlin

--- trying-kotlin/build.gradle.kts
+++ trying-kotlin/build.gradle.kts
@@ -2,5 +2,9 @@ plugins {
     alias(libs.plugins.kotlin.compiler)
 }
 
+repositories {
+    mavenCentral()
+}
+
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/tryingKotlin"))
