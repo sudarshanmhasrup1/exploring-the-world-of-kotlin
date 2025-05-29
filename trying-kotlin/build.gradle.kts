08297e66c678d9a4784206222af6c2c35e77f534
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-29 15:44:47 +0530

    Set JVM target to 11 for Java and Kotlin source code

--- trying-kotlin/build.gradle.kts
+++ trying-kotlin/build.gradle.kts
@@ -1,9 +1,20 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.compiler)
 }
 
 repositories {
     mavenCentral()
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_11
 }
 
 // Custom build directory
