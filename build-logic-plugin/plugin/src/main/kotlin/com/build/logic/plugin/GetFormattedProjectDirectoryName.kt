aeaf936c3c89128fd34092316ad7d809f43c0270
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-05-31 19:58:36 +0530

    Create GetFormattedProjectDirectoryName.kt

---
+++ build-logic-plugin/plugin/src/main/kotlin/com/build/logic/plugin/GetFormattedProjectDirectoryName.kt
@@ -0,0 +1,31 @@
+package com.build.logic.plugin
+
+fun getFormattedProjectDirectoryName(projectDirectoryName: String): String {
+
+    val nameSeparatedByDashList = projectDirectoryName.split("-")
+    var formattedName = ""
+
+    nameSeparatedByDashList.forEachIndexed { index, name ->
+        formattedName += if (index == 0) {
+            name.lowercase()
+        } else {
+            getStringWithFirstLetterCapitalized(value = name)
+        }
+    }
+
+    return formattedName
+}
+
+private fun getStringWithFirstLetterCapitalized(value: String): String {
+
+    var formattedString = ""
+    value.forEachIndexed { index, character ->
+        formattedString += if (index == 0) {
+            character.uppercase()
+        } else {
+            character.lowercase()
+        }
+    }
+
+    return formattedString
+}
