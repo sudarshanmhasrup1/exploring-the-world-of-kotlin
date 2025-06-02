58d89156073cffdb81a74a1b552a8c24b61ac7a1
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-02 19:52:21 +0530

    Update TypeInference.kt with added comments

--- trying-kotlin/src/main/kotlin/com/trying/kotlin/variables/TypeInference.kt
+++ trying-kotlin/src/main/kotlin/com/trying/kotlin/variables/TypeInference.kt
@@ -4,6 +4,8 @@ package com.trying.kotlin.variables
 fun main() {
     // Redundant way
     val firstName: String = "Sudarshan"
+
+    // Better way with type inference
     val lastName = "Mhasrup"
 
     println("My name is $firstName $lastName.")
