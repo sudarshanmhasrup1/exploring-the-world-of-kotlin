6efc327fbbb7e990ae91fe95d66090ddcc2e301f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-05 14:36:36 +0530

    Define Ktlint for build-logic/plugin module

--- build-logic/plugin/build.gradle.kts
+++ build-logic/plugin/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
     alias(libs.plugins.java.gradle)
@@ -12,10 +13,12 @@ version = libs.versions.buildLogicPlugin.version.get()
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
     implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.ktlint.gradle.plugin)
     testImplementation(libs.kotlin.test)
 }
 
@@ -26,6 +29,21 @@ gradlePlugin {
             version = libs.versions.buildLogicPlugin.version.get()
             implementationClass = libs.versions.buildLogicPlugin.implementationClass.get()
         }
+    }
+}
+
+val ktlintRuleSet = mapOf(
+    "ktlint_standard_final-newline" to "disabled",
+    "ktlint_standard_no-empty-file" to "disabled",
+    "ktlint_standard_trailing-comma-on-call-site" to "disabled",
+)
+
+ktlint {
+    android = false
+    ignoreFailures = false
+    additionalEditorconfig.set(ktlintRuleSet)
+    reporters {
+        reporter(reporterType = ReporterType.HTML)
     }
 }
 
