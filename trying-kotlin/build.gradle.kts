30aa41d434da9f1ae30332290652ab76bd7ed93f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-05 14:37:24 +0530

    Use extension functions to configure build logic

--- trying-kotlin/build.gradle.kts
+++ trying-kotlin/build.gradle.kts
@@ -1,25 +1,13 @@
-import com.build.logic.config.configureJvmTargetTo17
-import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+import com.build.logic.config.configureGroupIdAndVersion
+import com.build.logic.config.configureJvmTargetTo11
+import com.build.logic.config.configureKtlint
 
 plugins {
-    alias(libs.plugins.kotlin.compiler)
     alias(libs.plugins.build.logic)
+    alias(libs.plugins.kotlin.compiler)
     alias(libs.plugins.ktlint)
 }
 
-val ktlintRuleSet = mapOf(
-    "ktlint_standard_final-newline" to "disabled",
-    "ktlint_standard_no-empty-file" to "disabled",
-    "ktlint_standard_trailing-comma-on-call-site" to "disabled",
-)
-
-ktlint {
-    android = false
-    ignoreFailures = false
-    additionalEditorconfig.set(ktlintRuleSet)
-    reporters {
-        reporter(reporterType = ReporterType.HTML)
-    }
-}
-
-configureJvmTargetTo17(enableToolchain = true)
+configureGroupIdAndVersion()
+configureKtlint()
+configureJvmTargetTo11(enableToolchain = true)
