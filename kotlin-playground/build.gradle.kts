b932782a444eef7cdea5f232ac1d638a23301a6e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-05 22:22:11 +0530

    Setup kotlin-playground module to use kotlin jvm environment

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -1,3 +1,10 @@
-plugins {
+import com.build.logic.config.configureJvmTargetTo11
+import com.build.logic.config.configureKtlint
 
+plugins {
+    alias(libs.plugins.build.logic)
+    alias(libs.plugins.kotlin.compiler)
 }
+
+configureKtlint()
+configureJvmTargetTo11(enableToolchain = true)
