eb48a94f893e9b0b55208034c3fe2c02141cf594
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-05 23:17:06 +0530

    Add "com.android.library" plugin to project

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -1,3 +1,18 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
+
+kotlin {
+    androidTarget()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+}
