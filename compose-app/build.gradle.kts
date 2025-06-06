cd0fff3c4ebef768ca1cca7f7ed2c82a0aeacc98
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-06 15:12:51 +0530

    Apply "org.jetbrains.kotlin.plugin.compose" plugin compose-app module

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.multiplatform)
 }
 
 kotlin {
@@ -16,6 +17,12 @@ kotlin {
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+
         }
     }
 }
