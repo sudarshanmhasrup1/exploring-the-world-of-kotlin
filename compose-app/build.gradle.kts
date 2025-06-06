f984435afbe1f27da3ef25eed9a528679c163fe1
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-06 15:02:40 +0530

    Configure target platforms

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -2,3 +2,25 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
 }
+
+kotlin {
+    androidTarget()
+
+    jvm(name = "desktop")
+
+    listOf(
+        iosArm64(),
+        iosX64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+}
+
+android {
+    namespace = "com.namespace"
+    compileSdk = 35
+}
