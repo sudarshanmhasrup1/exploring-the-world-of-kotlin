a8d1621b931426408e112b8b738d931159bf9851
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-08 11:21:13 +0530

    Create compose app entry point file

---
+++ compose-app/src/commonMain/kotlin/com/compose/app/ComposeApp.kt
@@ -0,0 +1,15 @@
+package com.compose.app
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Main() {
+    
+}
+
+@Preview
+@Composable
+private fun MainPreview() {
+
+}
