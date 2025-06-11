ad122d56b3498b6e5a6b5efd00d3c99f32e450c3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-11 08:42:02 +0530

    Remove unwanted lines

--- kotlin-playground/build.gradle.kts
+++ kotlin-playground/build.gradle.kts
@@ -1,6 +1,5 @@
 import com.build.logic.config.configureJvmTargetTo11
 import com.build.logic.config.configureKtlint
-
 plugins {
     alias(libs.plugins.build.logic)
     alias(libs.plugins.kotlin.compiler)
