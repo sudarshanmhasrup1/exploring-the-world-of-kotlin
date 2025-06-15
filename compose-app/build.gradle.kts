8cd994729a75303900f671a1b5fd8bcca09a857e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-15 13:40:36 +0530

    Add "cafe.adriel.voyager:voyager-navigator" dependency to compose-app module

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
+            implementation(libs.voyager.navigator)
             implementation(libs.compose.multiplatform.ui)
             implementation(libs.compose.multiplatform.ui.tooling.preview)
         }
