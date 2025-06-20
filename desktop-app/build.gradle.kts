dc9804cdb4d6188136e19d892e9bad4a9f42e238
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-20 20:36:18 +0530

    Define desktop app module mainClass to build.gradle.kts

--- desktop-app/build.gradle.kts
+++ desktop-app/build.gradle.kts
@@ -1,3 +1,20 @@
-plugins {
+import com.build.logic.config.configureGroupIdAndVersion
+import com.build.logic.config.configureJvmTargetTo11
+import com.build.logic.config.configureKtlint
 
+plugins {
+    alias(libs.plugins.build.logic)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.kotlin.compiler)
+    alias(libs.plugins.compose.multiplatform)
+}
+
+configureGroupIdAndVersion()
+configureKtlint()
+configureJvmTargetTo11(enableToolchain = true)
+
+compose.desktop {
+    application {
+        mainClass = "com.cmp.desktop.MainAppKt"
+    }
 }
