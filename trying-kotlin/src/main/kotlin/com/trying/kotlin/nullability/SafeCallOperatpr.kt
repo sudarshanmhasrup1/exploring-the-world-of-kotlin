ac685599440952910cf2db722c386c704516746b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-20 16:03:01 +0530

    Improve code style

--- trying-kotlin/src/main/kotlin/com/trying/kotlin/nullability/SafeCallOperatpr.kt
+++ trying-kotlin/src/main/kotlin/com/trying/kotlin/nullability/SafeCallOperatpr.kt
@@ -1,6 +1,10 @@
 package com.trying.kotlin.nullability
 
-@Suppress("UNNECESSARY_SAFE_CALL", "VariableInitializerIsRedundant")
+@Suppress(
+    "UNNECESSARY_SAFE_CALL",
+    "VariableInitializerIsRedundant",
+    "VARIABLE_WITH_REDUNDANT_INITIALIZER"
+)
 fun main() {
     var username: String? = null
     username = "sudarshanmhasrup"
