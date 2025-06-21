bd85af352257f757bf91eac36425d3462a48c561
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-21 11:44:07 +0530

    Create entry point for desktop app

---
+++ compose-app/src/desktopMain/kotlin/com/compose/app/DesktopApp.kt
@@ -0,0 +1,11 @@
+package com.compose.app
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class DesktopApp : Screen {
+    @Composable
+    override fun Content() {
+
+    }
+}
